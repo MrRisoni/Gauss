05636ed1f09de4f738a6274f72c534553d62cb5b
MrRisoni <vanguelisk@gmail.com>
2015-05-06 12:22:41 +0300

    department entity

--- Entities/departments.cpp
+++ Entities/departments.cpp
@@ -9,4 +9,24 @@ Departments::~Departments()
 {
 
 }
+QString Departments::getDepName() const
+{
+    return DepName;
+}
+
+void Departments::setDepName(const QString &value)
+{
+    DepName = value;
+}
+int Departments::getDepID() const
+{
+    return DepID;
+}
+
+void Departments::setDepID(int value)
+{
+    DepID = value;
+}
+
+
 
--- Entities/departments.h
+++ Entities/departments.h
@@ -1,12 +1,24 @@
 #ifndef DEPARTMENTS_H
 #define DEPARTMENTS_H
 
+#include <QString>
 
 class Departments
 {
 public:
     Departments();
     ~Departments();
+
+
+    QString getDepName() const;
+    void setDepName(const QString &value);
+
+    int getDepID() const;
+    void setDepID(int value);
+
+private:
+    int DepID;
+    QString DepName;
 };
 
 #endif // DEPARTMENTS_H
