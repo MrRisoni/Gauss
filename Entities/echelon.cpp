825d914b9c4bf17b7bacb8301e2b89d6f2adfc82
MrRisoni <vanguelisk@gmail.com>
2015-05-10 19:18:32 +0300

    echelon entity

--- Entities/echelon.cpp
+++ Entities/echelon.cpp
@@ -9,4 +9,24 @@ Echelon::~Echelon()
 {
 
 }
+int Echelon::getExpYears() const
+{
+    return ExpYears;
+}
+
+void Echelon::setExpYears(int value)
+{
+    ExpYears = value;
+}
+int Echelon::getTeachers() const
+{
+    return Teachers;
+}
+
+void Echelon::setTeachers(int value)
+{
+    Teachers = value;
+}
+
+
 
--- Entities/echelon.h
+++ Entities/echelon.h
@@ -7,6 +7,16 @@ class Echelon
 public:
     Echelon();
     ~Echelon();
+
+    int getExpYears() const;
+    void setExpYears(int value);
+
+    int getTeachers() const;
+    void setTeachers(int value);
+
+private:
+    int ExpYears;
+    int Teachers;
 };
 
 #endif // ECHELON_H
