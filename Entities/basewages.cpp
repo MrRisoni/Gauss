c2de869cd7dc748d876ca4c3deb54b15952242e2
MrRisoni <vanguelisk@gmail.com>
2015-05-10 19:18:19 +0300

    base wage entity

--- Entities/basewages.cpp
+++ Entities/basewages.cpp
@@ -9,4 +9,34 @@ BaseWages::~BaseWages()
 {
 
 }
+Echelon BaseWages::getE() const
+{
+    return E;
+}
+
+void BaseWages::setE(const Echelon &value)
+{
+    E = value;
+}
+QDate BaseWages::getD() const
+{
+    return D;
+}
+
+void BaseWages::setD(const QDate &value)
+{
+    D = value;
+}
+float BaseWages::getWage() const
+{
+    return Wage;
+}
+
+void BaseWages::setWage(float value)
+{
+    Wage = value;
+}
+
+
+
 
--- Entities/basewages.h
+++ Entities/basewages.h
@@ -1,12 +1,28 @@
 #ifndef BASEWAGES_H
 #define BASEWAGES_H
 
+#include <QDate>
+#include "echelon.h"
+
 
 class BaseWages
 {
 public:
     BaseWages();
     ~BaseWages();
+    Echelon getE() const;
+    void setE(const Echelon &value);
+
+    QDate getD() const;
+    void setD(const QDate &value);
+
+    float getWage() const;
+    void setWage(float value);
+
+private:
+    Echelon E;
+    QDate D;
+    float Wage;
 };
 
 #endif // BASEWAGES_H
