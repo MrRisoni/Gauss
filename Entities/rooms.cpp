eaeae2fe672ff1e2ea87b0ad183a6dd2ebd343c7
MrRisoni <vanguelisk@gmail.com>
2015-05-19 17:46:11 +0300

    room entity

--- Entities/rooms.cpp
+++ Entities/rooms.cpp
@@ -9,4 +9,44 @@ Rooms::~Rooms()
 {
 
 }
+int Rooms::getRoomID() const
+{
+    return RoomID;
+}
+
+void Rooms::setRoomID(int value)
+{
+    RoomID = value;
+}
+int Rooms::getBuildingID() const
+{
+    return BuildingID;
+}
+
+void Rooms::setBuildingID(int value)
+{
+    BuildingID = value;
+}
+QString Rooms::getName() const
+{
+    return Name;
+}
+
+void Rooms::setName(const QString &value)
+{
+    Name = value;
+}
+int Rooms::getCapacity() const
+{
+    return Capacity;
+}
+
+void Rooms::setCapacity(int value)
+{
+    Capacity = value;
+}
+
+
+
+
 
--- Entities/rooms.h
+++ Entities/rooms.h
@@ -1,12 +1,31 @@
 #ifndef ROOMS_H
 #define ROOMS_H
 
+#include <QString>
+
 
 class Rooms
 {
 public:
     Rooms();
     ~Rooms();
+    int getRoomID() const;
+    void setRoomID(int value);
+
+    int getBuildingID() const;
+    void setBuildingID(int value);
+
+    QString getName() const;
+    void setName(const QString &value);
+
+    int getCapacity() const;
+    void setCapacity(int value);
+
+private:
+    int RoomID;
+    int BuildingID;
+    QString Name;
+    int Capacity;
 };
 
 #endif // ROOMS_H
