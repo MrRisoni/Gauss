4ddb364f9206b9463b3a75a4dcf24a9c01f89db0
MrRisoni <vanguelisk@gmail.com>
2015-05-19 17:45:53 +0300

    building entity

--- Entities/buildings.cpp
+++ Entities/buildings.cpp
@@ -9,4 +9,24 @@ Buildings::~Buildings()
 {
 
 }
+int Buildings::getBuildingID() const
+{
+    return BuildingID;
+}
+
+void Buildings::setBuildingID(int value)
+{
+    BuildingID = value;
+}
+QString Buildings::getName() const
+{
+    return Name;
+}
+
+void Buildings::setName(const QString &value)
+{
+    Name = value;
+}
+
+
 
--- Entities/buildings.h
+++ Entities/buildings.h
@@ -1,12 +1,22 @@
 #ifndef BUILDINGS_H
 #define BUILDINGS_H
 
+#include <QString>
 
 class Buildings
 {
 public:
     Buildings();
     ~Buildings();
+    int getBuildingID() const;
+    void setBuildingID(int value);
+
+    QString getName() const;
+    void setName(const QString &value);
+
+private:
+    int BuildingID;
+    QString Name;
 };
 
 #endif // BUILDINGS_H
