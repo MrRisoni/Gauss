b72b40fe284ec563f158d5f64be898a724cc9974
MrRisoni <vanguelisk@gmail.com>
2015-05-19 17:46:28 +0300

    Versicherung entity

--- Entities/versicherung.h
+++ Entities/versicherung.h
@@ -1,12 +1,18 @@
 #ifndef VERSICHERUNG_H
 #define VERSICHERUNG_H
 
+#include "teacher.h"
+#include "kassen.h"
 
 class Versicherung
 {
 public:
     Versicherung();
     ~Versicherung();
+private:
+    Teacher T;
+    Kassen K;
+
 };
 
 #endif // VERSICHERUNG_H
