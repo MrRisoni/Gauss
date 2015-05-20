01d4b9aef1e11c05b9fd0c9e70edf51f6a563c45
MrRisoni <vanguelisk@gmail.com>
2015-05-20 18:03:53 +0300

    schule request entity

--- Entities/requestschule.cpp
+++ Entities/requestschule.cpp
@@ -9,4 +9,64 @@ RequestSchule::~RequestSchule()
 {
 
 }
+Courses RequestSchule::getC() const
+{
+    return C;
+}
+
+void RequestSchule::setC(const Courses &value)
+{
+    C = value;
+}
+int RequestSchule::getCourseID() const
+{
+    return CourseID;
+}
+
+void RequestSchule::setCourseID(int value)
+{
+    CourseID = value;
+}
+int RequestSchule::getStudentID() const
+{
+    return StudentID;
+}
+
+void RequestSchule::setStudentID(int value)
+{
+    StudentID = value;
+}
+Members RequestSchule::getStudent() const
+{
+    return student;
+}
+
+void RequestSchule::setStudent(const Members &value)
+{
+    student = value;
+}
+QDate RequestSchule::getDat() const
+{
+    return dat;
+}
+
+void RequestSchule::setDat(const QDate &value)
+{
+    dat = value;
+}
+QString RequestSchule::getComments() const
+{
+    return comments;
+}
+
+void RequestSchule::setComments(const QString &value)
+{
+    comments = value;
+}
+
+
+
+
+
+
 
--- Entities/requestschule.h
+++ Entities/requestschule.h
@@ -2,11 +2,41 @@
 #define REQUESTSCHULE_H
 
 
+#include "courses.h"
+#include "members.h"
+
+
 class RequestSchule
 {
 public:
     RequestSchule();
     ~RequestSchule();
+    Courses getC() const;
+    void setC(const Courses &value);
+
+    int getCourseID() const;
+    void setCourseID(int value);
+
+    int getStudentID() const;
+    void setStudentID(int value);
+
+    Members getStudent() const;
+    void setStudent(const Members &value);
+
+    QDate getDat() const;
+    void setDat(const QDate &value);
+
+    QString getComments() const;
+    void setComments(const QString &value);
+
+private:
+    Courses C;
+    int CourseID;
+    int StudentID;
+    Members student;
+    QDate dat;
+    QString comments;
+
 };
 
 #endif // REQUESTSCHULE_H
