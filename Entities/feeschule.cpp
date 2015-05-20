cf656d603ee61c41f78f75a3ae98c105e141b03a
MrRisoni <vanguelisk@gmail.com>
2015-05-20 18:04:08 +0300

    schule fee entity

--- Entities/feeschule.cpp
+++ Entities/feeschule.cpp
@@ -9,4 +9,34 @@ FeeSchule::~FeeSchule()
 {
 
 }
+Courses FeeSchule::getC() const
+{
+    return C;
+}
+
+void FeeSchule::setC(const Courses &value)
+{
+    C = value;
+}
+QDate FeeSchule::getDat() const
+{
+    return dat;
+}
+
+void FeeSchule::setDat(const QDate &value)
+{
+    dat = value;
+}
+float FeeSchule::getFee() const
+{
+    return fee;
+}
+
+void FeeSchule::setFee(float value)
+{
+    fee = value;
+}
+
+
+
 
--- Entities/feeschule.h
+++ Entities/feeschule.h
@@ -1,12 +1,28 @@
 #ifndef FEESCHULE_H
 #define FEESCHULE_H
 
+#include "courses.h"
+#include <QDate>
 
 class FeeSchule
 {
 public:
     FeeSchule();
     ~FeeSchule();
+    Courses getC() const;
+    void setC(const Courses &value);
+
+    QDate getDat() const;
+    void setDat(const QDate &value);
+
+    float getFee() const;
+    void setFee(float value);
+
+private:
+    Courses C;
+    QDate dat;
+    float fee;
+
 };
 
 #endif // FEESCHULE_H
