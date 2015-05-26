553c479c80f10636f09187a1d8e4feb43ec7081a
MrRisoni <vanguelisk@gmail.com>
2015-05-26 18:27:53 +0300

    set a mongodb connection

--- E_Receipts/py_scripts/SetUpMongo.py
+++ E_Receipts/py_scripts/SetUpMongo.py
@@ -1,4 +1,9 @@
-import pymongo
+from pymongo import MongoClient
+
+
+def setUpDB():
+    connection = MongoClient()  ## connects by default to db at localhost:27017
+    return connection.Friedrich.Unterschreiben
 
 
 
