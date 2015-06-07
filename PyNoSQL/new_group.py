449e452638eaf4a1cd7fc7bfbde42e38226c5e0d
MrRisoni <vanguelisk@gmail.com>
2015-06-08 00:07:00 +0300

    python no sql tests

---
+++ PyNoSQL/new_group.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 23:50:50 2015
+
+@author: linguine
+"""
+
+insert group
+
+
+
+insert to absent
+
+
+
+insert a sohuld be payed
+
+update shouldbepayed 
